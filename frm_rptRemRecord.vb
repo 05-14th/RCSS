7fca547c26bbc851aa9ecf1df5a23306b1e69b3f
Christian Francisco <caffrancisco@gmail.com>
2024-03-10 04:29:45 +0800

    1

--- frm_rptRemRecord.vb
+++ frm_rptRemRecord.vb
@@ -2,11 +2,19 @@
 Public Class frm_rptRemRecord
 
     Private Sub frm_rptRemRecord_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim myrpt As New rpt_Remitance
         Loadrecord()
+        myrpt.Database.Tables("rcss_remittance").SetDataSource(dt)
+        CrystalReportViewer1.ReportSource = Nothing
+        CrystalReportViewer1.ReportSource = myrpt
+
     End Sub
     Sub Loadrecord()
         Try
-
+            cn.Open()
+            cm = New MySqlCommand("SELECT * FROM rcss_remittance", cn)
+            da = New MySqlDataAdapter(cm)
+            da.Fill(dt)
 
 
             cn.Close()
